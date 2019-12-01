8964f26884d4ae1d0594978e1e433130211dcfd1
asyard <asyard@gmail.com>
2019-12-01 20:04:57 +0300

    some fixes and design improvements

--- src/main/java/org/yardimci/hue/gui/lampconfig/LampConfigGUI.java
+++ src/main/java/org/yardimci/hue/gui/lampconfig/LampConfigGUI.java
@@ -225,12 +225,13 @@ public class LampConfigGUI extends JDialog {
 
                 //======== generalPanel ========
                 {
-                    generalPanel.setBorder ( new javax . swing. border .CompoundBorder ( new javax . swing. border .TitledBorder ( new javax . swing. border .EmptyBorder
-                    ( 0, 0 ,0 , 0) ,  "JF\u006frm\u0044es\u0069gn\u0065r \u0045va\u006cua\u0074io\u006e" , javax. swing .border . TitledBorder. CENTER ,javax . swing. border
-                    .TitledBorder . BOTTOM, new java. awt .Font ( "D\u0069al\u006fg", java .awt . Font. BOLD ,12 ) ,java . awt
-                    . Color .red ) ,generalPanel. getBorder () ) ); generalPanel. addPropertyChangeListener( new java. beans .PropertyChangeListener ( ){ @Override public void
-                    propertyChange (java . beans. PropertyChangeEvent e) { if( "\u0062or\u0064er" .equals ( e. getPropertyName () ) )throw new RuntimeException( )
-                    ;} } );
+                    generalPanel.setBorder (new javax. swing. border. CompoundBorder( new javax .swing .border .TitledBorder (new javax. swing
+                    . border. EmptyBorder( 0, 0, 0, 0) , "JF\u006frmDes\u0069gner \u0045valua\u0074ion", javax. swing. border. TitledBorder
+                    . CENTER, javax. swing. border. TitledBorder. BOTTOM, new java .awt .Font ("D\u0069alog" ,java .
+                    awt .Font .BOLD ,12 ), java. awt. Color. red) ,generalPanel. getBorder( )) )
+                    ; generalPanel. addPropertyChangeListener (new java. beans. PropertyChangeListener( ){ @Override public void propertyChange (java .beans .PropertyChangeEvent e
+                    ) {if ("\u0062order" .equals (e .getPropertyName () )) throw new RuntimeException( ); }} )
+                    ;
                     generalPanel.setLayout(null);
 
                     //---- nameLabel ----
@@ -396,7 +397,7 @@ public class LampConfigGUI extends JDialog {
                         }
                     });
                     lightControlPanel.add(brightnessSlider);
-                    brightnessSlider.setBounds(135, 125, 225, brightnessSlider.getPreferredSize().height);
+                    brightnessSlider.setBounds(130, 125, 230, brightnessSlider.getPreferredSize().height);
 
                     //---- colourLabel ----
                     colourLabel.setText(bundle.getString("label.colour"));
